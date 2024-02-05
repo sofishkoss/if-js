21ee5e973c3a57678b1421176e8adc8fa3995c77
sofishkosss <145139511+sofishkosss@users.noreply.github.com>
2024-02-05 18:50:21 +0300

    5/2/24

---
+++ main.js
@@ -0,0 +1,13 @@
+// let user= 'John Doe' ;
+// alert(user);
+//
+// let student = "Sofia";
+// alert(student);
+//
+// let user = student;
+// alert(user)
+
+let message;
+message = 'Hello!';
+
+alert(message);
