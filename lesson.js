2aa2051edf84d0aadfa1df666e5c4cd8a1b6fb12
sofishkosss <145139511+sofishkosss@users.noreply.github.com>
2024-02-21 19:15:38 +0300

    21/2/24

---
+++ lesson.js
@@ -0,0 +1,9 @@
+function sum(color){
+    const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+    // for (let i = 0; i < colors.length; i++) {
+    //     let a = document.getElementById("text1");
+    //     a.style.color = colors[i];
+    function onclick=document.getElementById('text1').style.color = '0';
+
+    }
+}
