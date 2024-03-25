0539ef8fef2e6515ebec2d65b14b0655082cdab4
sofishkosss <145139511+sofishkosss@users.noreply.github.com>
2024-03-25 17:15:36 +0300

    25/3/24

---
+++ lesson-1-перебор массива.js
@@ -0,0 +1,51 @@
+// let date =new Date('2020-11-26');
+// console.log(date.toLocaleDateString());
+
+
+
+const data = [
+    {
+        country: 'Russia',
+        city: 'Saint Petersburg',
+        hotel: 'Hotel Leopold',
+    },
+    {
+        country: 'Spain',
+        city: 'Santa Cruz de Tenerife',
+        hotel: 'Apartment Sunshine',
+    },
+    {
+        country: 'Slowakia',
+        city: 'Vysokie Tatry',
+        hotel: 'Villa Kunerad',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hostel Friendship',
+    },
+    {
+        country: 'Indonesia',
+        city: 'Bali',
+        hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+        country: 'Netherlands',
+        city: 'Rotterdam',
+        hotel: 'King Kong Hostel',
+    },
+    {
+        country: 'Marocco',
+        city: 'Ourika',
+        hotel: 'Rokoko Hotel',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+];
+
+data.forEach(function(item, i, data) {
+    alert( i + ": " + item + " (массив:" + data + ")" );
+});
